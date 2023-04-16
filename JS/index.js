07fa826fafdfd2ef5062595d00eecdfc4f2320f3
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:14:18 -0700

    session 6 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,38 @@
-var City= "Oslo" , Country = "Norway" , Continent = "Europe";
-alert(Country);
+var x = 30;
+var y = 30;
 
-function test(x , y , z){
-    return( x / y + z)
+var inhtml = document.getElementById("resault");
+inhtml.innerHTML = x + y;
+
+var z = "17";
+var t = "3";
+z = Number(z);
+t = Number(t);
+inhtml.innerHTML = z + t;
+z = parseInt(z);
+t = parseInt(t);
+inhtml.innerHTML = z + t;
+
+function tst() {
+    var x = 100;
+    var y = 200;
+    return x + y;
 }
-var s = 20;
-var k = 30;
+document.write(tst());
 
-document.getElementById("resault").innerHTML = s + k;
+function test(x, y, z) {
+    alert(x + y * z);
+}
+
+test(20 , 5 , 10);
+
+var me = {
+    name : "amirmohammad" ,
+    family: "hasanabadi" ,
+    age: 21,
+    isMan: true,
+    fullname: function(){
+        return "Your Fullname is " + me.name +" " +  this.family;
+    }
+}
+alert(me.fullname());
