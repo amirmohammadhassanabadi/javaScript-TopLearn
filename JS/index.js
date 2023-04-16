c1f9ef9f40788e8d52b133237100fd8688090f43
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:17:33 -0700

    session 6 - 3

--- JS/index.js
+++ JS/index.js
@@ -1,38 +1,49 @@
-var x = 30;
-var y = 30;
-
-var inhtml = document.getElementById("resault");
-inhtml.innerHTML = x + y;
-
-var z = "17";
-var t = "3";
-z = Number(z);
-t = Number(t);
-inhtml.innerHTML = z + t;
-z = parseInt(z);
-t = parseInt(t);
-inhtml.innerHTML = z + t;
-
-function tst() {
-    var x = 100;
-    var y = 200;
-    return x + y;
-}
-document.write(tst());
-
-function test(x, y, z) {
-    alert(x + y * z);
+function Event1() {
+    alert("Your Event Worked");
 }
 
-test(20 , 5 , 10);
-
-var me = {
-    name : "amirmohammad" ,
-    family: "hasanabadi" ,
-    age: 21,
-    isMan: true,
-    fullname: function(){
-        return "Your Fullname is " + me.name +" " +  this.family;
-    }
+function resault(x, y) {
+    var Sim = document.getElementById("resault");
+    Sim.innerHTML = x + y;
 }
-alert(me.fullname());
+
+var x = 100;
+if(x<=20){
+    alert("You Choosed the right Number");
+}else if(x<=30){
+    alert("Not a Bad Choise");
+}else if(x<=40){
+    alert("It isn't a Very Good Idea");
+}else{
+    alert("You Choosed the Wrong Number")
+}
+
+var x = 20
+switch(x){
+    case 10: alert("This is Number 10");
+    break;
+    case 20: alert("This is Number 20");
+    break;
+    case 30: alert("This is Number 30");
+    break;
+    case 40: alert("This is Number 40");
+    break;
+}
+
+document.write(new Date().getDay())
+switch(new Date().getDay()){
+    case 0: alert("This the 1st day of the week");
+    break;
+    case 1: alert("This the 2nd day of the week");
+    break;
+    case 2: alert("This the 3rd day of the week");
+    break;
+    case 3: alert("This the 4th day of the week");
+    break;
+    case 4: alert("This the 5th day of the week");
+    break;
+    case 5: alert("This the 6th day of the week");
+    break;
+    case 6: alert("This the 7th day of the week");
+    break;
+}
