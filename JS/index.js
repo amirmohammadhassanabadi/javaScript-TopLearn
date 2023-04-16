6f410bc979e16044e272b00f1ff6c8c61f3a4211
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:35:42 -0700

    session 7 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,33 @@
-var Array1 = ["Tom", "Hardy", 37, "English"];
-var txt = "";
-for(var i = 0; i<Array1.length ; i++){
-    txt += Array1[i] + "<br>";
-}
-document.getElementById("resault").innerHTML = txt;
-
-for (x in Array1) {
-  console.log(Array1[x]);
+for(var i = 0; i<=10; i++){
+    document.write(i + "<br>");
+    if(i===7){
+        break;
+    }
 }
 
-var z = 10;
+document.write("<hr>")
 
-while (z < 20) {
-  document.write(z + 10 + "<br>");
-  z++;
+for(var i = 0; i<=10; i++){
+    if(i===7){
+        break;
+    }
+    document.write(i + "<br>");
 }
 
-do {
-console.log(z);
-} while (z > 20);
+document.write("<hr>")
+
+for(var i = 0; i<=10; i++){
+    if(i===7){
+        continue;
+    }
+    document.write(i + "<br>");
+}
+
+document.write("<hr>")
+
+for(var i = 0; i<=10; i++){
+    document.write(i + "<br>");
+    if(i===7){
+        continue;
+    }
+}
