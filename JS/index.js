bc90516b8c80c4d4d9a3b0809e6bc2b62121a913
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:30:29 -0700

    session 6 - 5

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,21 @@
-function Dates(){
-    document.getElementById("BTN").innerHTML = Date();
+var Array1 = ["Tom", "Hardy", 37, "English"];
+var txt = "";
+for(var i = 0; i<Array1.length ; i++){
+    txt += Array1[i] + "<br>";
 }
+document.getElementById("resault").innerHTML = txt;
+
+for (x in Array1) {
+  console.log(Array1[x]);
+}
+
+var z = 10;
+
+while (z < 20) {
+  document.write(z + 10 + "<br>");
+  z++;
+}
+
+do {
+console.log(z);
+} while (z > 20);
