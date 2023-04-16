4c104b3be2f4a5a1e554f9f46019e8760f2e5f0c
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:04:39 -0700

    session 5 - 2

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,17 @@ var Hieght = 1.73;
 var Male = true;
 var Email = null;
 var City = "";
+
+alert(Name);
+alert(Age);
+alert(Hieght);
+alert(Male);
+alert(Email);
+alert(City);
+
+alert(typeof Name);
+alert(typeof Age);
+alert(typeof Hieght);
+alert(typeof Male);
+alert(typeof Email);
+alert(typeof City);
