2a8d1346d2275d50fd020f4b99316ce847548ec2
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-16 10:07:26 -0700

    session 5 - 4

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,10 @@
-function MyShow() {
-    var x = 100;
-    var y = 200;
-    var result = x + y;
-    alert(result);
-}
+var information = ["AmirMohammad", "HasanAbadi", 21, 1.73, true, null, ""];
+// alert(information[2]);
+var Data = new Array("AmirMohammad", "HasanAbadi", 21, 1.73, true, null, "");
+alert(Data[3]);
 
-MyShow();
+ document.write(information.sort());
+// alert(information[2]);
+// console.log(information.sort());
+ document.write("<hr>");
+document.write("Wellcome " + information[3]);
