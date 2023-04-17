74163d80126b89b5db7a459829b680ee0730e2be
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-17 08:01:07 -0700

    session 9 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,16 @@
-document.getElementById("P-1").innerHTML = "JavaScript DOM";
-
-document.getElementsByClassName("head")[0].innerHTML = "JavaScript DOM";
-
-document.getElementsByTagName("h3")[2].innerHTML = "JavaScript DOM";
-
-document.getElementsByName("text")[0].innerHTML = "JavaScript DOM";
-
-document.querySelectorAll("h3.test")[0].innerHTML = "JavaScript DOM";
+function animation() {
+    var animator = document.getElementById("myAnimate");
+    var start = 0;
+    var left = 0;
+    var animate = setInterval(go, 5);
+    function go() {
+        if (start == 450) {
+            clearInterval(animate);
+        } else {
+            start++;
+            left++;
+            animator.style.top = start;
+            animator.style.left = left;
+        }
+    }
+}
