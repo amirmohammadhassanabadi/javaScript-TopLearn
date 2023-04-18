a0b3f82968dd66936b7e5cc9a5fdf9aab0ab5eab
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-18 12:16:38 -0700

    session 19

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,12 @@
-var myImage = document.querySelector("img");
-document.querySelector("button").onclick = function() {
-    let mySrc = myImage.getAttribute("src");
-    if(mySrc == "img/1.jpg"){
-        myImage.setAttribute("src","img/2.jpg");
-    }
-    if(mySrc == "img/2.jpg"){
-        myImage.setAttribute('src','img/3.jpg');
-    }
-    if(mySrc == "img/3.jpg"){
-        myImage.setAttribute('src','img/1.jpg');
-    }
+var username = prompt("please tell us your name");
+var head = document.querySelector("h2");
+head.textContent = "you entered your name successfully";
+if(username == ""){
+    head.textContent = "try again";
+}
+if(username == " "){
+    head.textContent = "try again";
+}
+if(username = null){
+    head.textContent = "try again";
 }
