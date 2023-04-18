78509c3828318f5d11b5b49524a0b4c16edbcd87
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-18 11:34:28 -0700

    session 10 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,8 @@
 var container = document.getElementById("container");
-var head4 = document.createElement("h1");
-var TXT = document.createTextNode("Python");
-var head2 = document.getElementById("head2");
-container.appendChild(head4);
-head4.appendChild(TXT);
-container.insertBefore(head4, head2);
+var head1 = document.getElementById("head-1");
+var NewH1 = document.createElement("h2");
+var innerTXT = document.createTextNode("It's a replaced text");
+NewH1.appendChild(innerTXT);
+container.replaceChild(NewH1, head1);
+var head2 = document.getElementById("head-2");
+container.removeChild(head2);
