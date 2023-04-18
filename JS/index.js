62302cbd128a83c10804e6813b538144b64f114c
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-18 11:48:53 -0700

    session 10 - 3

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,18 @@
-var container = document.getElementById("container");
-var head1 = document.getElementById("head-1");
-var NewH1 = document.createElement("h2");
-var innerTXT = document.createTextNode("It's a replaced text");
-NewH1.appendChild(innerTXT);
-container.replaceChild(NewH1, head1);
-var head2 = document.getElementById("head-2");
-container.removeChild(head2);
+var myWidth = window.innerWidth;
+var myHeight = window.innerHeight;
+var myWidth2 = document.documentElement.clientWidth;
+var myHeight2 = document.documentElement.clientHeight;
+var myWidth3 = document.body.clientWidth;
+var myHeight3 = document.body.clientHeight;
+
+document.getElementById("head-1").innerHTML = "<span class='text-secondary'>window.innerWidth: </span>" + myWidth + " <span class='text-warning'>px</span>";
+document.getElementById("head-2").innerHTML = "<span class='text-secondary'>window.innerHeight: </span>" + myHeight + " <span class='text-warning'>px</span>";
+
+document.getElementById("head-3").innerHTML = "<span class='text-secondary'>document.documentElement.clientWidth: </span>" + myWidth2 + " <span class='text-warning'>px</span>";
+document.getElementById("head-4").innerHTML = "<span class='text-secondary'>document.documentElement.clientHeight: </span>" + myHeight2 + " <span class='text-warning'>px</span>";
+
+document.getElementById("head-5").innerHTML = "<span class='text-secondary'>document.body.clientWidth: </span>" + myWidth3 + " <span class='text-warning'>px</span>";
+document.getElementById("head-6").innerHTML = "<span class='text-secondary'>document.body.clientHeight: </span>" + myHeight3 + " <span class='text-warning'>px</span>";
+
+document.getElementById("head-7").innerHTML = "<span class='text-secondary'>window.location.href: </span>" + `<span class='text-warning'>${window.location.href}</span>`;
+document.getElementById("head-8").innerHTML = "<span class='text-secondary'>window.location.protocol: </span>" + `<span class='text-warning'>${window.location.protocol}</span>`;
