f781a5e2738e100342e497a5c047ff4f91cf5e90
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-18 11:53:15 -0700

    session 10 - 4

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,18 @@
-var myWidth = window.innerWidth;
-var myHeight = window.innerHeight;
-var myWidth2 = document.documentElement.clientWidth;
-var myHeight2 = document.documentElement.clientHeight;
-var myWidth3 = document.body.clientWidth;
-var myHeight3 = document.body.clientHeight;
+function goback() {
+    window.history.back();
+}
 
-document.getElementById("head-1").innerHTML = "<span class='text-secondary'>window.innerWidth: </span>" + myWidth + " <span class='text-warning'>px</span>";
-document.getElementById("head-2").innerHTML = "<span class='text-secondary'>window.innerHeight: </span>" + myHeight + " <span class='text-warning'>px</span>";
+function goforward() {
+    window.history.forward();
+}
 
-document.getElementById("head-3").innerHTML = "<span class='text-secondary'>document.documentElement.clientWidth: </span>" + myWidth2 + " <span class='text-warning'>px</span>";
-document.getElementById("head-4").innerHTML = "<span class='text-secondary'>document.documentElement.clientHeight: </span>" + myHeight2 + " <span class='text-warning'>px</span>";
-
-document.getElementById("head-5").innerHTML = "<span class='text-secondary'>document.body.clientWidth: </span>" + myWidth3 + " <span class='text-warning'>px</span>";
-document.getElementById("head-6").innerHTML = "<span class='text-secondary'>document.body.clientHeight: </span>" + myHeight3 + " <span class='text-warning'>px</span>";
-
-document.getElementById("head-7").innerHTML = "<span class='text-secondary'>window.location.href: </span>" + `<span class='text-warning'>${window.location.href}</span>`;
-document.getElementById("head-8").innerHTML = "<span class='text-secondary'>window.location.protocol: </span>" + `<span class='text-warning'>${window.location.protocol}</span>`;
+function popup() {
+    var txt = "";
+    var name = prompt("please enter your name", "Default Value");
+    if (name == "" || name == null || name == "Default Value") {
+        txt = "Name is not acceptable";
+    } else {
+        txt = "Wellcome dear " + name;
+    }
+    document.getElementById("h3").innerHTML = txt
+}
