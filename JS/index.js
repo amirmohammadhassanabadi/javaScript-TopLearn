9534613f311bebab290edd368fa3c4f0e5ee3bab
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-20 11:52:56 -0700

    session 25

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,20 @@
-let Num1 = prompt("Please enter number 1");
-let Num2 = prompt("Please enter number 2");
-document.write(Number(Num1) + Number(Num2));
-document.write(parseInt(Num1) + parseInt(Num2));
-document.write(parseFloat(Num1) + parseFloat(Num2));
-
-var x = false;
-x = String(x);
-document.write(typeof x);
-let z = "phrase";
-z = Number(z);
-console.log(typeof (z));
-document.write(z);
-
-let y = 10;
-y++;
-y++;
-y++;
-y++;
-y--;
-y += 20;
-y -= 3;
-y *= 3;
-y /= 5;
-y %= 3;
-console.log(y);
-
-let word1 = "amir";
-let word2 = "";
-word1 = Boolean(word1);
-word2 = Boolean(word2);
-console.log(word1);
-console.log(word2);
+// Array
+var Fruits = new Array("Apple" , "Cherry" , "Orange");
+var Fruits = ["Apple", "Cherry", "Orange"];
+Fruits[0] = "Watermelon";
+let Arrays = "Amir-Meyti-Hosein-Mamad-Ashkan";
+Arrays = Arrays.split("-");
+document.write(Fruits);
+document.write("<br>");
+console.log(Fruits.indexOf("Orange"));
+document.write(Fruits[1]);
+document.write("<br>");
+Fruits.push("Banana");
+Fruits.push("25");
+Fruits.sort();
+document.write(Fruits);
+document.write("<br>");
+console.log(typeof Fruits);
+document.write(Arrays);
+document.write("<br>");
+document.write(typeof Arrays);
