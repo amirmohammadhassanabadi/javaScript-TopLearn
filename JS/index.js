94b6a307f56fab1e954dd0f5fbe66d2121209fc1
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-20 11:31:12 -0700

    session 22

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,32 @@
-var Num = 10;
-switch (Num) {
-    case 1:
-        console.log("you choosed 1");
-        break;
+//data types
+let str = 'Amir';
+let number = 84;
+let gpa = 65.3;
+let bool = true;
+let myVar1 = null; //no value at all
+let myVar2 = undefined; //no value yet
 
-    case 2:
-        console.log("you choosed 2");
-        break;
+//document.write()
+document.write("<h2 class='text-danger'>This is by JS</h2>");
+document.write("<hr class='text-danger'>");
 
-    case 3:
-        console.log("you choosed 3");
-        break;
+var phrase = "Strings are cool";
+console.log(phrase);
+console.log(phrase.length);
+console.log(phrase.toUpperCase());
+console.log(phrase.toLocaleLowerCase());
+console.log(phrase.charAt(0));
+console.log(phrase.indexOf("o"));
+console.log(phrase.lastIndexOf("z"));
+console.log(phrase.substring(2,9));//from 2 to 8
+console.log(phrase.substring(phrase.indexOf("a"),phrase.length));
+console.log(phrase.substring(8,16));
+console.log(phrase.endsWith("d"));//false
+console.log(phrase.endsWith("cool"));//true
+console.log(phrase.includes("cool"));//true
+console.log(phrase.includes("z"));//false
 
-    case 4:
-        console.log("you choosed 4");
-        break;
-
-    default:
-        console.log("not a good choise");
-}
+//Concatenation
+let str1 = "Amir";
+let str2=  "mohammad";
+document.write(str1 + " " + str2)
