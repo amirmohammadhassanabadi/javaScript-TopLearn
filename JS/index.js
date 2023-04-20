3b97db2aea67b4c841d7e6717c3c3119e9dc670e
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-20 11:50:06 -0700

    session 24 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,33 @@
-// Dynamic Data Type
-var x = "name";
-x = "something";
-x = 35;
-let phrase = "They called me 'Amir'";
-console.log(phrase);
+let Num1 = prompt("Please enter number 1");
+let Num2 = prompt("Please enter number 2");
+document.write(Number(Num1) + Number(Num2));
+document.write(parseInt(Num1) + parseInt(Num2));
+document.write(parseFloat(Num1) + parseFloat(Num2));
 
-console.log(typeof (x));
+var x = false;
+x = String(x);
+document.write(typeof x);
+let z = "phrase";
+z = Number(z);
+console.log(typeof (z));
+document.write(z);
+
+let y = 10;
+y++;
+y++;
+y++;
+y++;
+y--;
+y += 20;
+y -= 3;
+y *= 3;
+y /= 5;
+y %= 3;
+console.log(y);
+
+let word1 = "amir";
+let word2 = "";
+word1 = Boolean(word1);
+word2 = Boolean(word2);
+console.log(word1);
+console.log(word2);
