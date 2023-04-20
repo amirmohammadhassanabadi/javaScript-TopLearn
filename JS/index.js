35ffecf33f5cd117424698e7de5e3ec89961fed3
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-20 11:36:43 -0700

    session 24 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,32 +1,8 @@
-var Num1 = -10.348;
-var Num2 = 10.348;
-var Num3 = 10.848;
-function printD(x) {
-    document.write(x);
-}
+// Dynamic Data Type
+var x = "name";
+x = "something";
+x = 35;
+let phrase = "They called me 'Amir'";
+console.log(phrase);
 
-printD(Num1);
-printD("<br>")
-printD(Math.abs(Num1));
-printD("<br>")
-printD(Math.max(Num1, 61));
-printD("<br>")
-printD(Math.min(Num1, 61));
-printD("<br>")
-printD(Math.round(Num2));
-printD("<br>")
-printD(Math.ceil(Num2));
-printD("<br>")
-printD(Math.floor(Num3));
-printD("<br>")
-printD(Math.pow(Num3, 2));
-printD("<br>")
-printD(Math.sqrt(Num3));
-printD("<br>")
-printD(Math.cbrt(Num3));
-printD("<br>")
-printD(Math.random() * 10);
-printD("<br>")
-printD(Math.round(Math.random() * 100));
-printD("<br>")
-printD(Math.sin(60));
+console.log(typeof (x));
