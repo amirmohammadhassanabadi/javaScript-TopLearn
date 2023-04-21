0a4c244c36bee30c37e63c78c0216b34b8a2666c
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 01:38:18 -0700

    session 29 - chat room mini-project

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,10 @@
-let Btn = document.getElementById("btn");
-Btn.addEventListener("mouseover", hover);
-function hover() {
-    Btn.style.backgroundColor = "black";
-    Btn.style.color = "dodgerblue";
-    Btn.style.border = "0";
-}
-Btn.addEventListener("mouseout", BtnOut);
-function BtnOut() {
-    Btn.style.backgroundColor = "dodgerblue";
-    Btn.style.color = "black";
-    Btn.style.border = "0";
-}
-Btn.addEventListener("click", BtnClick);
-function BtnClick() {
-    Btn.style.backgroundColor = "salmon";
-    Btn.style.color = "white";
-    Btn.style.border = "0";
+var innerInput = document.getElementById("input");
+let buttom = document.getElementById("btn");
+buttom.addEventListener("click", onPage);
+function onPage() {
+    let newLi = document.createElement("li");
+    newLi.innerHTML = innerInput.value;
+    let ul = document.querySelector("ul");
+    ul.appendChild(newLi);
+    innerInput.value = ""
 }
