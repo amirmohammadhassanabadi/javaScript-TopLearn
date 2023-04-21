26e2b7753c1d424202182e1d5205050026444513
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 02:26:25 -0700

    session 36 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,25 +1,7 @@
-var Questions = [
-    {
-        prompt: "Where is the capital of Netherlands?\n(a) Rotterdam\n(b) Amsterdam\n(c) Utrecht",
-        answer: "b"
-    },
-    {
-        prompt: "Where is the capital of Denmark?\n(a) Copenhagen\n(b) Aarhus\n(c) Aalborg",
-        answer: "a"
-    },
-    {
-        prompt: "Which one these cities isn't in South Africa?\n(a) Cape Town\n(b) Johannesburg\n(c) Cairo",
-        answer: "c"
-    }
-]
-var score = 0;
-for (var i = 0; i < Questions.length; i++) {
-    let ask = prompt(Questions[i].prompt);
-    if (ask == Questions[i].answer) {
-        score++;
-        alert("Your answer was right")
-    } else {
-        alert("Your answer was not right")
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 3; j++) {
+        for (let k = 1; k <= 2; k++) {
+            document.write("i =  " + i + "  ,   j =  " + j + "  ,   k =  " + k + "<br>");
+        }
     }
 }
-alert("your score is : " + score + " of " + Questions.length);
