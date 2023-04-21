0634ef2cfd66dd3f03eac8b29399c7d9e904c0fa
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 02:27:09 -0700

    session 36 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,16 @@
-for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 3; j++) {
-        for (let k = 1; k <= 2; k++) {
-            document.write("i =  " + i + "  ,   j =  " + j + "  ,   k =  " + k + "<br>");
+var Num = [
+    [1, [2, 3], 4],
+    [[5, 6, 7], 8, [9, 10]],
+    [11, 12, 13]
+]
+for (var i = 0; i < Num.length; i++) {
+    for (var j = 0; j < Num[i].length; j++) {
+        for (var k = 0; k < Num[i][j].length; k++) {
+            if (Num[i][j].length > 0) {
+                document.write(Num[i][j][k] + "<br>")
+            } else {
+                document.write(Num[i][j] + "<br>")
+            }
         }
     }
 }
