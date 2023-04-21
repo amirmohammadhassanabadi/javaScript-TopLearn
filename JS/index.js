e3bb38bc731510bd7ae8795b0ce25baaa316817c
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 02:05:06 -0700

    session 33

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,16 @@
-var i = 1;
-while (i <= 10) {
-    document.write(i + "<br>");
-    i++;
+let password = "Abcd1234"
+let guess;
+let counter = 0;
+let error = false;
+while (password != guess && error == false) {
+    if (counter < 3) {
+        guess = prompt("Please enter your password");
+        counter++;
+        if (password == guess) {
+            alert("you entered successfully")
+        }
+    } else {
+        alert("You're not allowed to enter for 2 hours");
+        error = true;
+    }
 }
