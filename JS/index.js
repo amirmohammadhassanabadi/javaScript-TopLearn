b76edd0454331c3cd849c00c8f345aa89de827dc
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 01:51:11 -0700

    session 30

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,12 @@
-var innerInput = document.getElementById("input");
-let buttom = document.getElementById("btn");
-buttom.addEventListener("click", onPage);
-function onPage() {
-    let newLi = document.createElement("li");
-    newLi.innerHTML = innerInput.value;
-    let ul = document.querySelector("ul");
-    ul.appendChild(newLi);
-    innerInput.value = ""
+let wrapper = document.getElementById("wrapper");
+let Car = {
+    Brand: "Ferrari",
+    Model: "Roma",
+    Price: "350,000$",
+    PrintInfo: function () {
+        console.log(this.Brand + " " + this.Model + ` is ${this.Price}`);
+        return `${this.Brand} ${this.Model} is ${this.Price}`
+    }
 }
+
+wrapper.innerHTML = Car.PrintInfo()
