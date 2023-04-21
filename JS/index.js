a1c18b7ea3209c7aa81c638a24e785ced265745f
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 02:07:42 -0700

    session 34 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,10 @@
-var i = 0;
+const fruits = ["Apple", "Banana", "Orange"];
+for (x of fruits) {
+    document.write(x + "<br>");
+}
+// -----------------------------------------------------
+let i = 0;
 while (i < 10) {
     document.write(i + "<br>");
-    i++;
-}
-for (var i = 0; i < 10; i++) {
-    document.write(i + "<br>");
-}
-var Friends = ['Mamad', "Mina", "Sogol"];
-for (var i = 0; i < Friends.length; i++) {
-    document.write("Number " + (i + 1) + " " + "is " + Friends[i] + "<br>");
+    i = i + 2;
 }
