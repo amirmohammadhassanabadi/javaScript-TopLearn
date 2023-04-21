7d9192111192d5afac20bd14a4338b977286549b
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 02:03:30 -0700

    session 32

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,5 @@
-let Movie = {
-    Title: "Outlaw King",
-    InitialRelease: "September 6, 2018",
-    Director:{
-        FullName:"David Mackenzie",
-        BirthDay: "May 10, 1966",
-        Nationality: "Scottish"
-    },
-    Duration:"2h 17m",
-    Badget: "120 million USD",
-    Casts: [{
-        FullName:"Chris Pine",
-        BirthDay: "August 26, 1980",
-        Nationality: "American"
-    },
-    {
-        FullName:"Florence Pugh",
-        BirthDay: "January 3, 1996",
-        Nationality: "English"
-    }]
+var i = 1;
+while (i <= 10) {
+    document.write(i + "<br>");
+    i++;
 }
-console.log(Movie.Casts[1].FullName);
