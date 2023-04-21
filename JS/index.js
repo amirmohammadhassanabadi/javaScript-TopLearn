e8c1c2ad494cc6223a12edcb5a68863ef4534654
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-21 01:01:10 -0700

    session 27 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,5 @@
-let head = document.getElementById("head");
-var Name1 = head.getAttribute("name");
-alert(Name1);
-head.style.color = "white";
-head.style.backgroundColor = "salmon"
-var newli = document.createElement("li");
-var newli2 = document.createElement("li");
-newli.innerHTML = "Sake";
-newli.innerHTML = "Raki";
-
 let ul = document.querySelector("ul");
-ul.appendChild(newli);
-var item2 = document.querySelectorAll("li")[1];
-ul.removeChild(item2);
+let item3 = document.querySelectorAll("li")[2];
+let newItem = document.createElement("li");
+newItem.innerHTML = "Sake";
+ul.replaceChild(newItem, item3);
