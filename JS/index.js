c817d1a2c9c1eec22cc0fac148a388e806a3cf74
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-25 13:28:04 -0700

    session 44 - 2

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,20 @@
-var x = "Amir", y = 12, z = "BMW";
-let country = "Finland", City = "Tokyo", Num = 34;
+myVar = 73;
+var myVar;
+console.log(myVar);
+// myVar2 = 68;
+// let myVar2;
+// console.log(myVar2);
 
-var x = 24;
-console.log(x);
-let city = "Dubai";
-{
-    let c = "Li";
-}
-console.log(c);
-{
-    var d = 15;
-}
-console.log(d);
+const Person = "Amir";
+// Person = "Nika";
+console.log(Person);
+const CarCompanies = ["Toyota", "Audi", "Jaguar", "Lamaborghini"];
+CarCompanies[1] = "Benz";
+console.log(CarCompanies[1]);
+CarCompanies.push("Bentley");
+document.write(CarCompanies);
+
+const myCar = { Brand: "Mercedes Benz", Model: "AMG GT", Price: 125000 };
+myCar.Price = 135000;
+console.log(myCar.Price);
+myCar.owner = "Amir";
