0ab81af5d707af3f8f3ef2608449302e2fb28072
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-25 13:30:23 -0700

    session 45 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,11 @@
-myVar = 73;
-var myVar;
-console.log(myVar);
-// myVar2 = 68;
-// let myVar2;
-// console.log(myVar2);
-
-const Person = "Amir";
-// Person = "Nika";
-console.log(Person);
-const CarCompanies = ["Toyota", "Audi", "Jaguar", "Lamaborghini"];
-CarCompanies[1] = "Benz";
-console.log(CarCompanies[1]);
-CarCompanies.push("Bentley");
-document.write(CarCompanies);
-
-const myCar = { Brand: "Mercedes Benz", Model: "AMG GT", Price: 125000 };
-myCar.Price = 135000;
-console.log(myCar.Price);
-myCar.owner = "Amir";
+function myFunc() {
+    var car = "BMW i8";
+}
+// console.log(car);
+action();
+console.log(carName);
+function action() {
+    carName = "Range Rover";
+}
+action();
+console.log(carName);
