df2aa539b2239c528594618289268727e3e76c49
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-25 13:17:41 -0700

    session 43 - 1

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,3 @@
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
-prev.onclick = function () {
-    window.history.back();
-}
-next.addEventListener("click", function () {
-    window.history.forward();
-})
-// -----------------------------------------------------------------------------
-if (confirm("Are you older than 18?")) {
-    alert("Wellcome");
-} else {
-    alert("You are not allowed");
-}
+var Head = document.getElementById("head");
+console.log(Head.parentNode);
+console.log(Head.parentNode.nodeName);
